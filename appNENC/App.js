09fa2ff27fd8fb6a1ddcb1335e3535792497f225
ggemelle <gabriel.spgemelli@gmail.com>
2024-08-21 10:41:52 -0300

    :sparkles: nova tela

--- appNENC/App.js
+++ appNENC/App.js
@@ -1,13 +1,42 @@
 import * as React from "react";
-import { Image, StyleSheet, View } from "react-native";
+import { Image, StyleSheet, View, TouchableOpacity, Text } from "react-native";
+import { useNavigation } from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import logoImage from "./assets/logoNenc.png"
 
+
+function HomeScreen({navigation}){
+  return (
+    <View style={styles.planoDeFundo}>
+      <TouchableOpacity onPress={() => navigation.navigate('NextScreen')}>
+        <Image style={styles.logoIcon} resizeMode="cover" source={logoImage} />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+function NextScreen(){
+  return (
+    <View style={stylesNextScreen.container}>
+      <Text style={stylesNextScreen.text}>Esta é a próxima tela!</Text>
+    </View>
+  );
+};
+
+
+const Stack = createNativeStackNavigator();
 const myApp = () => {
 
   return (
-    <View style={styles.planoDeFundo}>
-      <Image style={styles.logoIcon} resizeMode="cover" source={logoImage} />
-    </View>);
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="NextScreen" component={NextScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+
 };
 
 const styles = StyleSheet.create({
@@ -26,6 +55,19 @@ const styles = StyleSheet.create({
     alignItems:"center",
     justifyContent: "center"
   }
+});
+
+const stylesNextScreen = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
 });
 
 export default myApp;
