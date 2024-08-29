e2bb8f6c0b0076e6bfa84872f8205ed991576be2
IASR19 <itamar_augusto@ges.inatel.br>
2024-08-29 16:59:39 -0300

    :recycle: remove video

--- appNENC/pages/pageTres.js
+++ appNENC/pages/pageTres.js
@@ -8,53 +8,20 @@ import { useNavigation } from '@react-navigation/native'; // Importa o hook de n
 
 const PageTres = () => {
   const [mediaType, setMediaType] = React.useState('image');
-  const videoRef = React.useRef(null);
   const navigation = useNavigation(); // Instancia o hook de navegação
 
   React.useEffect(() => {
-    if (mediaType === 'video' && videoRef.current) {
-      videoRef.current.playAsync(); // Garante que o vídeo será reproduzido quando alternar para vídeo
-    }
-
     // Lógica para navegar para a próxima tela após um tempo
     const timeoutId = setTimeout(() => {
-      if (videoRef.current) {
-        videoRef.current.pauseAsync(); // Pausa o vídeo antes de navegar
-      }
       navigation.navigate('PageQuatro'); // Substitua 'PageQuatro' pelo nome da sua próxima tela
-    }, 30000); // 30000ms = 30 segundos
+    }, 1000); // 5000ms = 5 segundos
 
     return () => clearTimeout(timeoutId); // Limpa o timeout se o componente for desmontado ou se a mídia for alterada
-  }, [mediaType, navigation]);
-
-  const toggleMediaType = () => {
-    setMediaType(prevMediaType => prevMediaType === 'image' ? 'video' : 'image');
-  };
+  }, [navigation]);
 
   return (
     <View style={styles.page3}>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={toggleMediaType}
-        hitSlop={{ top: 20, bottom: 20, left: 20, right: 20 }} // Aumenta a área de toque do botão
-      >
-        <Text style={styles.comercial}>COMERCIAL</Text>
-      </TouchableOpacity>
-      {mediaType === 'image' ? (
-        <Image style={styles.logo} resizeMode="contain" source={Logo} />
-      ) : (
-        <Video
-          ref={videoRef}
-          source={VideoSource}
-          style={styles.video}
-          resizeMode="contain"
-          shouldPlay
-          isLooping
-          useNativeControls // Adiciona controles de mídia nativos
-          volume={1.0} // Garante que o volume esteja no máximo
-          isMuted={false} // Garante que o som não esteja mutado
-        />
-      )}
+      <Image style={styles.logo} resizeMode="contain" source={Logo} />
     </View>
   );
 };
@@ -68,18 +35,18 @@ const styles = StyleSheet.create({
     height: 40,
     width: 100,
   },
-  comercial: {
-    left: 0,
-    top: 8,
-    fontSize: 15,
-    fontWeight: "700",
-    fontFamily: "Inter-Bold",
-    color: "#fff",
-    textAlign: "center",
-    height: 30
-  },
+  //comercial: {
+    //left: 0,
+    //top: 8,
+    //fontSize: 15,
+    //fontWeight: "700",
+    //fontFamily: "Inter-Bold",
+    //color: "#fff",
+    //textAlign: "center",
+    //height: 30
+  //},
   page3: {
-    backgroundColor: "#000",
+    backgroundColor: "#fff",
     flex: 1,
     width: "100%",
     height: 640,
@@ -88,9 +55,12 @@ const styles = StyleSheet.create({
   logo: {
     width: 300,
     height: 300,
-    top: -30,
-    left: 220,
+    position: 'absolute',  // Certifique-se de que a imagem usa position absolute
+    top: '50%',            // Centraliza verticalmente
+    left: '50%',           // Centraliza horizontalmente
+    transform: [{ translateX: -150 }, { translateY: -150 }], // Ajusta o centro para o meio da imagem
   },
+  
   video: {
     width: 465,
     height: 465,
